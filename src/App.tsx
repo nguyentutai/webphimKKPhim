0c373e5a7837c47d8a183dc6aa91eb5798cdbf3d
tai19062004 <nguyentutai1964@gmail.com>
2024-05-28 17:31:43 +0700

    add code seo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import "../style/main.css"
-import "../style/reponsite.css"
-import Header from "./pages/HeaderPage"
-import { Route, Routes } from "react-router-dom"
-import DetailSingeMoviePage from "./pages/Detail/DetailSingeMoviePage"
-import HomePage from "./pages/HomePage"
-import DetailSeriesMovie from "./pages/Detail/DetailSeriesMovies"
-import DetailNewMovie from "./pages/Detail/DetailNewMovie"
-import ListMovieSinge from "./pages/ListMovie/ListMovieSinge"
-import ListMovieSeries from "./pages/ListMovie/ListMovieSeries"
-import ListMovieCarton from "./pages/ListMovie/ListMovieCarton"
-import ListTVShow from "./pages/ListMovie/ListTVShow"
-import FooterPage from "./pages/FooterPage"
-import ScrollToTop from "./components/ScrollPage"
+import "../style/main.css";
+import "../style/reponsite.css";
+import Header from "./pages/HeaderPage";
+import { Route, Routes, useLocation } from "react-router-dom";
+import DetailSingeMoviePage from "./pages/Detail/DetailSingeMoviePage";
+import HomePage from "./pages/HomePage";
+import DetailSeriesMovie from "./pages/Detail/DetailSeriesMovies";
+import DetailNewMovie from "./pages/Detail/DetailNewMovie";
+import ListMovieSinge from "./pages/ListMovie/ListMovieSinge";
+import ListMovieSeries from "./pages/ListMovie/ListMovieSeries";
+import ListMovieCarton from "./pages/ListMovie/ListMovieCarton";
+import ListTVShow from "./pages/ListMovie/ListTVShow";
+import FooterPage from "./pages/FooterPage";
+import ScrollToTop from "./components/ScrollPage";
+import NotFoundPage from "./pages/NotFoudPage";
 
 function App() {
+  const location = useLocation();
+  const isNotFoundPage = ![
+    '/home',
+    '/phimle',
+    '/phimhot',
+    '/phimchieurap',
+    '/phimbo',
+    '/phim-le',
+    '/phim-bo',
+    '/hoat-hinh',
+    '/phim-moi-cap-nhat',
+    '/phim',
+    '/tv-shows',
+    '/phim-le',
+    '/phim-bo',
+    '/hoat-hinh',
+    '/tv-shows'
+  ].some(path => location.pathname.startsWith(path));
 
   return (
     <>
-      <Header />
+      {!isNotFoundPage && <Header />}
       {/* Router */}
       <ScrollToTop />
       <Routes>
@@ -33,22 +52,15 @@ function App() {
         <Route path='/phim-moi-cap-nhat/:tenphim' element={<DetailNewMovie />} />
         <Route path='/phim/:tenphim' element={<DetailNewMovie />} />
         <Route path='/tv-shows/:tenphim' element={<DetailNewMovie />} />
-        <Route path='/phim-le' element={
-          <ListMovieSinge />
-        } />
-        <Route path='/phim-bo' element={
-          <ListMovieSeries />
-        } />
-        <Route path='/hoat-hinh' element={
-          <ListMovieCarton />
-        } />
-        <Route path='/tv-shows' element={
-          <ListTVShow />
-        } />
+        <Route path='/phim-le' element={<ListMovieSinge />} />
+        <Route path='/phim-bo' element={<ListMovieSeries />} />
+        <Route path='/hoat-hinh' element={<ListMovieCarton />} />
+        <Route path='/tv-shows' element={<ListTVShow />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <FooterPage />
+      {!isNotFoundPage && <FooterPage />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/NotFoudPage.tsx
@@ -0,0 +1,11 @@
+const NotFoudPage = () => {
+    return (
+        <div className="not-found-container container">
+            <h1 className="not-found-title">404</h1>
+            <p className="not-found-message">URL của nội dung này đã bị thay đổi hoặc không còn tồn tại.</p>
+            <a href="/" className="not-found-link">Go Back to Home</a>
+        </div>
+    )
+}
+
+export default NotFoudPage
